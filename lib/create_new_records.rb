80637c9884113beea8942434dac1016592c71675
Tim Habermaas <tim.habermaas@gmail.com>
2013-02-07 00:40:35 +0100

    fix bug where only one record type was updated at a time

--- lib/create_new_records.rb
+++ lib/create_new_records.rb
@@ -2,9 +2,9 @@ class CreateNewRecords
   def self.for(user, puzzle)
     singles = user.singles.for(puzzle).recent(RecordType.max_count)
 
-    RecordType.all.any? do |t|
+    RecordType.all.each do |t|
       self.for_type user, puzzle, singles[0..(t.count - 1)], t
-    end
+    end.any? { |e| e }
   end
 
   def self.for_type(user, puzzle, singles, type)
